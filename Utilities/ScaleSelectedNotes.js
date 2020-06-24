2f81ae9c53cbedfc7306867bac0673775346a5f9
teha921 <teha921@gmail.com>
2020-06-24 11:45:05 +0900

    Remove duration 0 notes. Fix formatting.

--- Utilities/ScaleSelectedNotes.js
+++ Utilities/ScaleSelectedNotes.js
@@ -2,10 +2,10 @@ var SCRIPT_TITLE = "Scale Selected Notes";
 
 function getClientInfo() {
   return {
-    "name" : SV.T(SCRIPT_TITLE),
-    "author" : "Dreamtonics",
-    "versionNumber" : 1,
-    "minEditorVersion" : 65537
+    "name": SV.T(SCRIPT_TITLE),
+    "author": "Dreamtonics",
+    "versionNumber": 1,
+    "minEditorVersion": 65537
   }
 }
 
@@ -13,8 +13,8 @@ function getTranslations(langCode) {
   if(langCode == "ja-jp") {
     return [
       ["Scale Selected Notes", "読み込まれたノートを拡大縮小"],
-      ["Enter scaling factor (Default: cancel truncation loss).", 
-      "拡大縮小係数を入力してください（既定：切り捨て誤差打ち消し）。"]
+      ["Enter scaling factor (Default: cancel truncation loss).",
+        "拡大縮小係数を入力してください（既定：切り捨て誤差打ち消し）。"]
     ];
   }
   return [];
@@ -24,28 +24,33 @@ function main() {
   var selection = SV.getMainEditor().getSelection();
   var selectedNotes = selection.getSelectedNotes();
 
-  var N = selectedNotes.length; 
+  var N = selectedNotes.length;
 
   var start = selectedNotes[0].getOnset();
-  var end = selectedNotes[N-1].getEnd();
+  var end = selectedNotes[N - 1].getEnd();
 
   var trueDurationEstimate = end - start + 0.5 * N * SV.QUARTER / 480;
-  var truncatedDurationEstimate = end - start; 
+  var truncatedDurationEstimate = end - start;
   var scale = trueDurationEstimate / truncatedDurationEstimate;
 
-  scale = SV.showInputBox(SV.T(SCRIPT_TITLE), 
-  SV.T("Enter scaling factor (Default: cancel truncation loss)."), scale);
+  scale = SV.showInputBox(SV.T(SCRIPT_TITLE),
+    SV.T("Enter scaling factor (Default: cancel truncation loss)."), scale);
 
   var prevEnd = -1;
   for(var i = 0; i < N; i ++) {
     var currOnset = selectedNotes[i].getOnset();
     var currEnd = selectedNotes[i].getEnd();
 
+    if(currOnset == currEnd) {
+      selectedNotes.removeNote(i);
+      break;
+    }
+
     if(i > 0 && prevEnd == currOnset)
       selectedNotes[i].setOnset(selectedNotes[i - 1].getEnd());
     else
       selectedNotes[i].setOnset(scale * currOnset);
-    
+
     selectedNotes[i].setDuration(scale * currEnd - selectedNotes[i].getOnset());
     prevEnd = currEnd;
   }
